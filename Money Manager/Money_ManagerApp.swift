a5622cbe647888aa11c2fe7f88f331dfa5fa1f2f
Alex Chan <chowing110@gmail.com>
2023-04-06 16:37:51 -0700

    Initial Commit

---
+++ Money Manager/Money_ManagerApp.swift
@@ -0,0 +1,17 @@
+//
+//  Money_ManagerApp.swift
+//  Money Manager
+//
+//  Created by Ho Wing CHAN on 2023-04-06.
+//
+
+import SwiftUI
+
+@main
+struct Money_ManagerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
